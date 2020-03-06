628f9deca44869c848a7b58a2dd57b809d0be807
Luke Encrapera <l.encrapera@gmail.com>
2020-03-06 10:59:40 -0500

    Initial commit

---
+++ addPSDexception.ps1
@@ -0,0 +1,13 @@
+﻿# Add user to Print Sync Download exception by employee ID as input
+$username = Read-Host "Username"
+$userdata = Get-ADUser $username
+$name = $userdata.Surname+', '+ $userdata.GivenName
+$name
+
+$PSDexception = '59c41a19-0b58-4775-bbba-aaa19ad7247e'
+
+$objectid = Get-AzureADUser -Filter "DisplayName eq '$name'"
+$objectid.ObjectId
+
+Add-AzureADGroupMember -ObjectId $PSDexception -RefObjectId $objectid.ObjectId
+Write-Host "Added $name"
